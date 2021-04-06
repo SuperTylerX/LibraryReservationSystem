437cd27731a082ea985a1fc9e9f28d59324bc745
bowen01 <bowen.yang@mail.concordia.ca>
2021-04-06 18:09:01 +0800

    Done order Part

--- Project3Core/src/main/java/pojo/Book.java
+++ Project3Core/src/main/java/pojo/Book.java
@@ -1,14 +1,6 @@
 package pojo;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement(name = "book")
-@XmlAccessorType(XmlAccessType.FIELD)
 public class Book {
-    @XmlElement
+
     int bookId;
     String googleId;
     String isbn;
